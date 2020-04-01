e7da86bc11b7285302eb047fba143df0e98e20ca
Jeff Hsu <sirjeffhsu@gmail.com>
2020-04-01 12:13:19 -0700

    Feat: docs folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -62,3 +62,24 @@ exports.onCreateNode = ({ node, actions, getNode }) => {
     })
   }
 }
+
+// use docs instead of dist for `gatsby build`
+const fs = require("fs")
+
+exports.onPreInit = () => {
+  if (process.argv[2] === "build") {
+    fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true })
+    fs.renameSync(
+      path.join(__dirname, "public"),
+      path.join(__dirname, "public_dev")
+    )
+  }
+}
+
+exports.onPostBuild = () => {
+  fs.renameSync(path.join(__dirname, "public"), path.join(__dirname, "docs"))
+  fs.renameSync(
+    path.join(__dirname, "public_dev"),
+    path.join(__dirname, "public")
+  )
+}
